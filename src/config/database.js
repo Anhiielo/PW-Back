2dab391f2ed6c2a88ad120699d8e24aa93d877f0
Anhiielo <angelobalbin333@gmail.com>
2026-07-13 14:03:58 -0500

    fix: arreglar SyntaxError de exportacion de sequelize que crasheaba el servidor

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from 'sequelize';
 
-export const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD,
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
     dialect: "postgres",
@@ -10,5 +10,7 @@ export const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER,
         rejectUnauthorized: false
       }
     }
-  })
+  });
+
+export default sequelize;
 
